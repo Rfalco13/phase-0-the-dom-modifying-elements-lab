79f644175a7ccc2ae31770f27f9e81f56206c192
ryanfalco13@gmail.com <ryanfalco13@gmail.com>
2023-05-09 02:20:47 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.textContent = "This is a new header";
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Falco is the champion";
+newHeader.setAttribute("id", "victory");
